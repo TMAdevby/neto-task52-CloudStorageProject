2182fdba72b81e5b676fd6ec51524694340cb84c
TMAdevby <mtipun@mail.ru>
2026-04-27 14:39:33 +0300

    Initial commit

---
+++ src/main/java/com/example/netotask52cloudstorageproject/NetoTask52CloudStorageProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.netotask52cloudstorageproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetoTask52CloudStorageProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetoTask52CloudStorageProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netotask52cloudstorageproject/NetoTask52CloudStorageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netotask52cloudstorageproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetoTask52CloudStorageProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
