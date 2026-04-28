ded404298d09266681163e15e73a5a2f3ea55d57
TMAdevby <mtipun@mail.ru>
2026-04-28 16:55:14 +0300

    file repository

--- src/main/java/com/example/netotask52cloudstorageproject/repository/FileRepository.java
+++ src/main/java/com/example/netotask52cloudstorageproject/repository/FileRepository.java
@@ -1,5 +1,21 @@
+
 package com.example.netotask52cloudstorageproject.repository;
 
-public class FileRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+import com.example.netotask52cloudstorageproject.model.StoredFile;
+import com.example.netotask52cloudstorageproject.model.User;
 
+public interface FileRepository extends JpaRepository<StoredFile, Long> {
+
+    List<StoredFile> findByUserOrderByUploadedAtDesc(User user);
+
+    Optional<StoredFile> findByUserAndFilename(User user, String filename);
+
+    boolean existsByUserAndFilename(User user, String filename);
+
+    Optional<StoredFile> findByFileHash(String fileHash);
+
+    void deleteByUserAndFilename(User user, String filename);
 }
