c54936f2eef7c2143ddfaed1fb1204dbee0c49f5
TMAdevby <mtipun@mail.ru>
2026-04-28 20:36:17 +0300

    dto2

--- src/main/java/com/example/netotask52cloudstorageproject/dto/LoginResponse.java
+++ src/main/java/com/example/netotask52cloudstorageproject/dto/LoginResponse.java
@@ -1,4 +1,20 @@
+
 package com.example.netotask52cloudstorageproject.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Data
+@NoArgsConstructor
+
 public class LoginResponse {
+
+    @JsonProperty("auth-token")
+    private String authToken;
+
+    public LoginResponse(String authToken) {
+        this.authToken = authToken;
+    }
 }
