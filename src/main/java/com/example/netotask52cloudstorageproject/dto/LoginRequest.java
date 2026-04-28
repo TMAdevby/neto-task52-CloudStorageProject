6857c402801f9772a2d46ec58b809e2ad3c764d5
TMAdevby <mtipun@mail.ru>
2026-04-28 20:30:11 +0300

    dto1

--- src/main/java/com/example/netotask52cloudstorageproject/dto/LoginRequest.java
+++ src/main/java/com/example/netotask52cloudstorageproject/dto/LoginRequest.java
@@ -1,4 +1,15 @@
+
 package com.example.netotask52cloudstorageproject.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+
+    private String login;
+    private String password;
 }
