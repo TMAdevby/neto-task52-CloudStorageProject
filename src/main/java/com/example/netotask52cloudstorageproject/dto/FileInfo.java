3c8007b13cafa6df67291bcf4e685e0c905b6b48
TMAdevby <mtipun@mail.ru>
2026-04-30 16:50:07 +0300

    FileInfo

--- src/main/java/com/example/netotask52cloudstorageproject/dto/FileInfo.java
+++ src/main/java/com/example/netotask52cloudstorageproject/dto/FileInfo.java
@@ -1,4 +1,24 @@
+
 package com.example.netotask52cloudstorageproject.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
 public class FileInfo {
+
+    private String filename;
+    private Long size;
+
+    public FileInfo(String filename, Long size) {
+        this.filename = filename;
+        this.size = size;
+    }
+
+    public static FileInfo fromEntity(com.example.netotask52cloudstorageproject.model.StoredFile file) {
+        return new FileInfo(file.getFilename(), file.getSize());
+    }
 }
