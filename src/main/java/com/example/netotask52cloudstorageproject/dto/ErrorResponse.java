21c693d9af240a24240e2c08f07814e1581014b6
TMAdevby <mtipun@mail.ru>
2026-04-30 16:52:06 +0300

    ErrorResponse

--- src/main/java/com/example/netotask52cloudstorageproject/dto/ErrorResponse.java
+++ src/main/java/com/example/netotask52cloudstorageproject/dto/ErrorResponse.java
@@ -1,4 +1,32 @@
+
 package com.example.netotask52cloudstorageproject.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
 public class ErrorResponse {
+
+    private String message;
+    private Integer id;
+
+    public ErrorResponse(String message, Integer id) {
+        this.message = message;
+        this.id = id;
+    }
+
+    public static ErrorResponse badRequest(String message) {
+        return new ErrorResponse(message, 400);
+    }
+
+    public static ErrorResponse unauthorized(String message) {
+        return new ErrorResponse(message, 401);
+    }
+
+    public static ErrorResponse internalError(String message) {
+        return new ErrorResponse(message, 500);
+    }
 }
