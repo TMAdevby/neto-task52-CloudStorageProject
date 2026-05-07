e723506bcae04efdeb1f899e8df275c58c4ba3c0
TMAdevby <mtipun@mail.ru>
2026-05-07 18:48:10 +0300

    finish

--- src/main/java/com/example/netotask52cloudstorageproject/controller/FileController.java
+++ src/main/java/com/example/netotask52cloudstorageproject/controller/FileController.java
@@ -25,7 +25,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cloud")
+@RequestMapping({"", "/cloud"})
 @RequiredArgsConstructor
 public class FileController {
 
